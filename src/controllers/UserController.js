6f932767937343c38a23d542fdb1250717c5fd4b
Diogo <di_smgomes@hotmail.com>
2023-10-03 14:46:34 -0300

    feat: User CRUD developed

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,78 @@ class UserController {
   async create(req, res) {
     try {
       const newUser = await User.create(req.body);
-      res.json(newUser);
+      return res.json(newUser);
     } catch (error) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const users = await User.findByPk(id);
+      return res.json(users);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ["Missing ID."],
+        });
+      }
+
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ["User not found"],
+        });
+      }
+
+      const updatedData = await user.update(req.body);
+      return res.json(updatedData);
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ["Missing ID."],
+        });
+      }
+
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ["User not found"],
+        });
+      }
+
+      await user.destroy();
+      return res.json(user);
+    } catch (error) {
+      return res.status(400).json({
         errors: error.errors.map((err) => err.message),
       });
     }
